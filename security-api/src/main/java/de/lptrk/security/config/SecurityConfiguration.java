6fc6e584e7b41eb5557dd3d42d4268d49b075a98
lptrk <lktpriv@gmail.com>
2023-09-20 21:32:22 +0200

    added react

--- security-api/src/main/java/de/lptrk/security/config/SecurityConfiguration.java
+++ security-api/src/main/java/de/lptrk/security/config/SecurityConfiguration.java
@@ -39,6 +39,7 @@ public class SecurityConfiguration {
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers(
+                        "",
                         "/api/v1/auth/**",
                         "/v2/api-docs",
                         "/v3/api-docs",
