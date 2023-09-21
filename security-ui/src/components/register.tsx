61b160191c77509acbcde0474570ab9535cdb462
lptrk <lktpriv@gmail.com>
2023-09-21 19:26:18 +0200

    added some styling

---
+++ security-ui/src/components/register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Register = () => {
+    return (
+        <div>
+            <button className='w-20 hover:bg-stone-400 rounded-full hover:bg-opacity-30 hover:font-bold'>Register</button>
+        </div>
+    )
+}
