c99e22aaffe3dcf403b87ee5259748779a1c4046
lptrk <lktpriv@gmail.com>
2023-09-22 09:07:20 +0200

    added routing funcitonality

--- security-ui/src/components/register.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Register = () => {
-    return (
-        <div>
-            <button className='w-20 hover:bg-stone-400 rounded-full hover:bg-opacity-30 hover:font-bold'>Register</button>
-        </div>
-    )
-}
